d17ca194d50ef2551ae21af812267619d95a1b5c
timur <timuric@gmail.com>
2024-03-20 10:11:41 +0100

    Revert "Revalidate bug"
    This reverts commit 296adbc18de1ce8510fa6d87c5cb42e04094ab3e.

--- src/app/(main)/todos/page.tsx
+++ src/app/(main)/todos/page.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 
-const Comments = async () => {
+export default async function Home() {
   await fetch("https://jsonplaceholder.typicode.com/comments/1");
-  return <></>;
-};
 
-const Todos = async () => {
   await fetch("https://jsonplaceholder.typicode.com/todos/1", {
     next: { revalidate: 1 },
   });
 
   return <></>;
-};
-export default async function Home() {
-  return (
-    <>
-      <Todos />
-      <Comments />
-    </>
-  );
 }
