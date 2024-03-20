296adbc18de1ce8510fa6d87c5cb42e04094ab3e
timur <timuric@gmail.com>
2024-03-20 10:09:24 +0100

    Revalidate bug

--- src/app/(main)/todos/page.tsx
+++ src/app/(main)/todos/page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-export default async function Home() {
+const Comments = async () => {
   await fetch("https://jsonplaceholder.typicode.com/comments/1");
+  return <></>;
+};
 
+const Todos = async () => {
   await fetch("https://jsonplaceholder.typicode.com/todos/1", {
     next: { revalidate: 1 },
   });
 
   return <></>;
+};
+export default async function Home() {
+  return (
+    <>
+      <Todos />
+      <Comments />
+    </>
+  );
 }
